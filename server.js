8261f7b83ce70fd12cdff28d579a884d2285773f
Rafal Monka <monka.rafal@gmail.com>
2020-05-06 21:49:26 +0200

    production first

--- server.js
+++ server.js
@@ -37,9 +37,9 @@ app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
 
-bj.backupJob();
-//var j = schedule.scheduleJob("0 22 * * *", function(){
-//  bj.backupJob();
-//});
+//bj.backupJob();
+var j = schedule.scheduleJob("0 22 * * *", function(){
+ bj.backupJob();
+});
 
 
